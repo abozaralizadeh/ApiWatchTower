2b19b9471204c36acb6501caa74d0c7a5f4f6643
abozar <abozar.alizadeh-miab@vodafone.com>
2020-12-10 19:46:48 +0100

    Update AD EndPoint

--- ApiWatchTower/settings.py
+++ ApiWatchTower/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'ApiWatchTower.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 DATABASES = {
-    'dev': {
+    'develop': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     },
